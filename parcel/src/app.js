6e7aa2157f05b66d7ce7fa3fa3281230dad5394b
vgrigoryevich <v.grigoryevich@gmail.com>
2019-08-05 20:55:52 +0300

    PARCAL

---
+++ parcel/src/app.js
@@ -0,0 +1,4 @@
+import short from 'shortid';
+export const handleShow = () => {
+    console.log(short());
+};
---
+++ parcel/src/root.js
@@ -0,0 +1,5 @@
+import * as fn from "./app"
+
+const btn = document.querySelector(".btn")
+
+btn.addEventListener("click", fn.handleShow)
